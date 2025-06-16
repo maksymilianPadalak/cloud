6b827b9bd8d61298d120d9ab0dcc4ce5db44ada1
maksymilianpadalak <maksymilian.padalak@gmail.com>
2025-06-16 17:46:41 +0200

    Change reverb's decay font color

--- src/components/Effects/ReverbPedal/ReverbPedal.vue
+++ src/components/Effects/ReverbPedal/ReverbPedal.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="root">
       <div class="pedalContent">
-        <h2 class="text-white">Decay: {{ reverb.decay }}</h2>
+        <h2>Decay: {{ reverb.decay }}</h2>
         <Knob v-model:effect-parameter="reverb.decay" />
       </div>
     </div>
@@ -39,9 +39,5 @@ const { reverb } = useAmplifier()
   align-items: center;
   justify-content: center;
   gap: 20px;
-}
-
-.text-white {
-  color: white;
 }
 </style>
