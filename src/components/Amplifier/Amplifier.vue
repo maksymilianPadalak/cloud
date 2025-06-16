8e5e626de03fcceeb74222bb07da49240429602d
maksymilianpadalak <maksymilian.padalak@gmail.com>
2025-06-16 17:58:00 +0200

    Add master Knob ui

--- src/components/Amplifier/Amplifier.vue
+++ src/components/Amplifier/Amplifier.vue
@@ -2,6 +2,10 @@
   <div>
     <div class="root">
       <div class="knobWrapper">
+        <h3>Gain: {{ gain }}</h3>
+        <Knob v-model:effect-parameter="gain" class="knobInput" />
+      </div>
+      <div class="knobWrapper">
         <h3>Bass: {{ bass }}</h3>
         <Knob v-model:effect-parameter="bass" class="knobInput" />
       </div>
@@ -14,8 +18,8 @@
         <Knob v-model:effect-parameter="treble" class="knobInput" />
       </div>
       <div class="knobWrapper">
-        <h3>Gain: {{ gain }}</h3>
-        <Knob v-model:effect-parameter="gain" class="knobInput" />
+        <h3>Master: {{ master }}</h3>
+        <Knob v-model:effect-parameter="master" class="knobInput" />
       </div>
     </div>
   </div>
@@ -24,19 +28,23 @@
 <script setup lang="ts">
 import Knob from '@/components/Knob'
 import { useAmplifier } from '@/composables/useAmplifier'
+import { ref } from 'vue'
 
 const { gain, bass, mid, treble } = useAmplifier()
+
+//TODO: Create the master volume control logic after UI is developed
+const master = ref(5.5)
 </script>
 
 <style scoped>
 .root {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-columns: repeat(5, 1fr);
   align-items: center;
   border: 5px solid lightblue;
   height: 250px;
   border-radius: 20px;
-  width: 600px;
+  width: 800px;
   padding: 20px;
 }
 
