fb87811ce6df82a2094e5ec82aa205796012c924
maksymilianpadalak <maksymilian.padalak@gmail.com>
2025-07-09 22:16:14 +0200

    Move audio playground to home view

--- src/routes.ts
+++ src/routes.ts
@@ -1,20 +1,21 @@
-import HomeView from '@/views/HomeView'
+import AudioPlaygroundView from '@/views/AudioPlaygroundView'
 import type { RouteRecordRaw } from 'vue-router'
 
 export const routes: Readonly<RouteRecordRaw[]> = [
   {
     path: '/',
     name: 'Home',
-    component: HomeView,
+    // component: HomeView,
+    component: AudioPlaygroundView,
   },
-  {
-    path: '/tuner',
-    name: 'Tuner',
-    component: () => import('@/views/TunerView/TunerView.vue'),
-  },
-  {
-    path: '/audio-playground',
-    name: 'AudioPlayground',
-    component: () => import('@/views/AudioPlaygroundView'),
-  },
+  // {
+  //   path: '/tuner',
+  //   name: 'Tuner',
+  //   component: () => import('@/views/TunerView/TunerView.vue'),
+  // },
+  // {
+  //   path: '/audio-playground',
+  //   name: 'AudioPlayground',
+  //   component: () => import('@/views/AudioPlaygroundView'),
+  // },
 ]
