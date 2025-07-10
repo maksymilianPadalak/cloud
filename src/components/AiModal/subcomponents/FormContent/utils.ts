bf2946197520af4c4511e95db820c0063e1e0c6b
maksymilianpadalak <maksymilian.padalak@gmail.com>
2025-07-10 10:24:49 +0200

    Update properties with Object.assign

--- src/components/AiModal/subcomponents/FormContent/utils.ts
+++ src/components/AiModal/subcomponents/FormContent/utils.ts
@@ -10,9 +10,10 @@ export const updateProcessorsParams = (newSoundParams: GrokSoundResponse) => {
     chorusProcessor,
   } = useAmplifier()
 
-  amplifierProcessor.params = newSoundParams.amplifier
-  delayProcessor.params = newSoundParams.delay
-  distortionProcessor.params = newSoundParams.distortion
-  reverbProcessor.params = newSoundParams.reverb
-  chorusProcessor.params = newSoundParams.chorus
+  //TODO: read about Object.assign
+  Object.assign(amplifierProcessor.params, newSoundParams.amplifier)
+  Object.assign(delayProcessor.params, newSoundParams.delay)
+  Object.assign(distortionProcessor.params, newSoundParams.distortion)
+  Object.assign(reverbProcessor.params, newSoundParams.reverb)
+  Object.assign(chorusProcessor.params, newSoundParams.chorus)
 }
